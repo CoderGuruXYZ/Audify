98e013df9c26ba3fee6f3a2a2cd8d0dde0ba4088
CoderGuruXYZ <singhlionuk@gmail.com>
2022-10-05 19:46:28 +0000

    Songs Generation Script

--- songs.js
+++ songs.js
@@ -3,47 +3,54 @@ var songs = [
         image: "blanknote.jpg",
         name: "Song Number 1",
         artist: "Artist Name",
-        duration: "1:48"
+        duration: "1:48",
+        file: "song.mp3"
     },
     {
         image: "blanknote.jpg",
         name: "Song Number 1",
         artist: "Artist Name",
-        duration: "1:48"
+        duration: "1:48",
+        file: "song.mp3"
     },
     {
         image: "blanknote.jpg",
         name: "Song Number 1",
         artist: "Artist Name",
-        duration: "1:48"
+        duration: "1:48",
+        file: "song.mp3"
     },
     {
         image: "blanknote.jpg",
         name: "Song Number 1",
         artist: "Artist Name",
-        duration: "1:48"
+        duration: "1:48",
+        file: "song.mp3"
     },
     {
         image: "blanknote.jpg",
-        name: "Song Number 1",
+        name: "DDU-DU DDU-DU",
         artist: "Artist Name",
-        duration: "1:48"
+        duration: "1:48",
+        file: "song.mp3"
     },
     {
         image: "blanknote.jpg",
-        name: "Song Number 1",
+        name: "Bunty Roll",
         artist: "Artist Name",
-        duration: "1:48"
+        duration: "1:48",
+        file: "song.mp3"
     },
     {
         image: "blanknote.jpg",
-        name: "Song Number 1",
+        name: "Stay OYEE",
         artist: "Artist Name",
-        duration: "1:48"
+        duration: "1:48",
+        file: "song.mp3"
     },
 ]
 
-function generateSong(id, image, name, artist, duration) {
+function generateSong(id, image, name, artist, duration, target) {
     var song = document.createElement("div");
     song.classList.add("song");
     song.id = id;
@@ -53,4 +60,45 @@ function generateSong(id, image, name, artist, duration) {
     songImage.src = "images/" + image;
 
     song.appendChild(songImage);
+
+    var songInfo = document.createElement("div");
+    songInfo.classList.add("songInfo");
+
+    var songTitle = document.createElement("div");
+    songTitle.classList.add("songTitle");
+    songTitle.innerHTML = name;
+    songTitle.style.fontSize = 225 / name.length + "px";
+
+    songInfo.appendChild(songTitle);
+
+    var songDetails = document.createElement("div");
+    songDetails.classList.add("songDetails");
+
+    var songArtist = document.createElement("songArtist");
+    songArtist.classList.add("songArtist");
+    songArtist.innerHTML = artist;
+
+    songDetails.appendChild(songArtist);
+
+    var songDuration = document.createElement("div");
+    songDuration.classList.add("songDuration");
+    songDuration.innerHTML = duration;
+
+    songDetails.appendChild(songDuration);
+
+    songInfo.appendChild(songDetails);
+
+    song.appendChild(songInfo);
+
+    target.appendChild(song);
 }
+
+function loadSongs() {
+    $(".songs").html("");
+
+    for(i = 0; i < songs.length; i++) {
+        generateSong(i, songs[i].image, songs[i].name, songs[i].artist, songs[i].duration, document.querySelector(".songs"));
+    }
+}
+
+loadSongs();
